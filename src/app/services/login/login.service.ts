d1513814159d3d3dc4e2f83357cdce04e3ec060b
Aishik Deb <debaishik14@gmail.com>
2021-05-28 21:19:19 +0530

    login service update & add product page

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,18 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private productsUrl = "https://electronics-backend1.herokuapp.com";
+  httpOptions = {
+    headers: new HttpHeaders({ 
+      'Content-Type': 'application/json'
+    })
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  loginUser(email: string, password: string): boolean{
-    if(email == "admin" && password == "admin"){
-      return true;
-    }
-    else{
-      return false;
-    }
+  loginUser(email: string, password: string): Observable<any>{
+    return this.http.post(this.productsUrl+'/login-user', {"userName": email, "password": password}, this.httpOptions)
+    .pipe(
+      tap(_ => console.log(`Login verification successfull`)),
+      catchError(this.handleError<any>('loginUser'))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
