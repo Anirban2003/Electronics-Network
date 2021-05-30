8cb6cd8719c84e140a8123a9bf1653cafcd46380
Aishik Deb <debaishik14@gmail.com>
2021-05-30 20:05:08 +0530

    move authgaurd to separate folder

--- src/app/services/authgaurd/authgaurd.service.ts
+++ src/app/services/authgaurd/authgaurd.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot,RouterStateSnapshot, UrlTree } from '@angular/router';
-import { LoginService } from './login/login.service';
+import { LoginService } from '../login/login.service';
 
 @Injectable({
   providedIn: 'root'
