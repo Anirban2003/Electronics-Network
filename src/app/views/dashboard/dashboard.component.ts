16241162b4ac9136e8e7e5308c8fc5ebff231004
Suchana123 <me.suchana.mitra@gmail.com>
2021-06-03 10:57:30 +0530

    Dashboard update

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { ElectronicsService } from '../../services/electronics/electronics.service';
 import { AppDialogComponent } from '../../feature/app-dialog/app-dialog.component';
+import {Product} from '../../product';
+import {MatTableDataSource} from '@angular/material/table';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -20,6 +22,7 @@ export class DashboardComponent implements OnInit {
   columnsToDisplay = ['id', 'name', 'description', 'quantity','price'];
   expandedElement: any;
   loader: boolean = false;
+  oldProduct! :Product[]
 
   constructor(
     public dialog: MatDialog,
@@ -34,8 +37,10 @@ export class DashboardComponent implements OnInit {
     this.loader = true;
     this.electronicsService.getProducts().subscribe(product=>{
       
-      this.dataSource=product.data;
+      this.oldProduct = product.data;
+      this.dataSource=new MatTableDataSource(this.oldProduct);
       this.expandedElement= product.data;
+      
 
       this.loader = false;
 
@@ -43,7 +48,7 @@ export class DashboardComponent implements OnInit {
     })
   }
 
-  openDialog(id:any) {
+  openDialog(prod:any) {
     const dialogRef = this.dialog.open(AppDialogComponent, {
       data: {
         title: "Are you sure that you want to delete this item?",
@@ -53,15 +58,24 @@ export class DashboardComponent implements OnInit {
       }
     });
     dialogRef.afterClosed().subscribe(result => {
-      //console.log(`Dialog result: ${result}`);
        if(result==true){
-         this.electronicsService.deleteProduct(id).subscribe(res => {
+         this.electronicsService.deleteProduct(prod.id).subscribe(res => {
           if(res.success==true) {
-            this.getAllProduct();
+            this.oldProduct=this.oldProduct.filter((t)=>t.id != prod.id);
+            this.dataSource = this.oldProduct;
           }
          })
        }
     });
+  }
+
+  applyFilter(event: Event) {
+
+    const filterValue = (event.target as HTMLInputElement).value;
+    console.log(filterValue);
+    console.log(this.dataSource);
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    console.log(this.dataSource.filter);
   }
 }
 
