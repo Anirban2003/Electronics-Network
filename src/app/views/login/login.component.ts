aacd6f16d8d03a59028ccadace99e9c113f215ad
Aishik Deb <debaishik14@gmail.com>
2021-06-03 13:06:53 +0530

    code refactor

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login/login.service';
 import { Router } from '@angular/router';
 import { AppDialogComponent } from 'src/app/feature/app-dialog/app-dialog.component';
-
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
@@ -11,25 +11,39 @@ import { AppDialogComponent } from 'src/app/feature/app-dialog/app-dialog.compon
 })
 export class LoginComponent implements OnInit {
 
-userName : any="";
-password : any="";
+  userName: any = "";
+  password: any = "";
+  loader: boolean = false;
 
-  constructor(private loginService : LoginService , private router: Router,) { }
+  constructor(private loginService: LoginService, private router: Router, public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
   onSubmit() {
-    console.log(this.userName);
-    console.log(this.password);
-    this.loginService.loginUser(this.userName,this.password)
-    .subscribe((res) => {
-      if(res.success == true){
-        console.log("Login Successfull");
-        this.router.navigate(["home"]);
-      }
-      else
-      console.log("Login failed");
-    })
-  }
+    this.loader = true;
+    this.loginService.loginUser(this.userName, this.password)
+      .subscribe((res) => {
+        if (res.success == true) {
+          this.loader = false;
+          console.log("Login Successfull");
+          this.router.navigate(["home"]);
+        }
+        else {
+          this.loader = false;
+          console.log("Login failed");
+          const dialogRef = this.dialog.open(AppDialogComponent, {
+            data: {
+              title: "Incorrect username or password",
+              secondaryButtonRequired: false,
+              primaryButtonText: "OK"
+            }
+          });
+          // dialogRef.afterClosed().subscribe(() => {
+          //   this.router.navigate(["dashboard"]);
+          // });
 
+        }
+      }
+    )
+  }
 }
